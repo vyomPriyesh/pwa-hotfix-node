b37d4291cff97b3d62f5a4c86dac6118624d35a5
Priyesh Bhanderi <vyompriyesh@gmail.com>
2025-11-15 11:35:52 +0530

    add new

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { assetsUrl, folderName } from "./Config.js";
 
 const app = express();
 const port = 3000;
-const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017";
+const DATABASE_URL = process.env.DATABASE_URL;
 
 connectDB(DATABASE_URL);
 app.use(express.json());
